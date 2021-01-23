e244b421636a9b97c7fa40ff195998bad0c221d4
梁波 <810669825@qq.com>
2021-01-23 20:17:17 +0800

    简易脚手架

---
+++ index.js
@@ -0,0 +1,36 @@
+const Generator = require('yeoman-generator')
+
+module.exports = class extends Generator {
+
+    prompting () {
+        return this.prompt([
+          {
+            type: 'input',
+            name: 'name',
+            message: 'Your project name',
+            default: this.appname
+          }
+        ])
+        .then(answers => {
+          this.answers = answers
+        })
+      }
+
+      writing () {
+        // 把每一个文件都通过模板转换到目标路径
+    
+        const templates = [
+          'index.html',
+          'main.css',
+          'main.js',
+        ]
+    
+        templates.forEach(item => {
+          this.fs.copyTpl(
+            this.templatePath(item),
+            this.destinationPath(item),
+            this.answers
+          )
+        })
+      }
+}
